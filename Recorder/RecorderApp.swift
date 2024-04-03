90eb639b8d499a97092e11ad1aa539a95faa38b8
Prateek Bhardwaj <prbhardwaj@microsoft.com>
2024-04-03 10:32:07 +0530

    Initial Commit

---
+++ Recorder/RecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecorderApp.swift
+//  Recorder
+//
+//  Created by Prateek Bhardwaj on 03/04/24.
+//
+
+import SwiftUI
+
+@main
+struct RecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
